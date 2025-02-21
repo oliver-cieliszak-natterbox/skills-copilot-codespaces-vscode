77cbae2c665eb08db97fd1804cb6971fc1f039d8
Oliver C <oliver.cieliszak@redmatter.com>
2025-02-21 10:22:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server
+// Start server
+// Load comments from file
+// Add new comment
+// Save comments to file
+// Stop server
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express()
